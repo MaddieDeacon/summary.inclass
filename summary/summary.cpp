e46506e3e686ad15040a224aba1dfd87c95dbd54
MaddieDeacon <125885771+MaddieDeacon@users.noreply.github.com>
2023-10-06 16:56:10 +0100

    Update summary.cpp

--- summary/summary.cpp
+++ summary/summary.cpp
@@ -24,9 +24,9 @@ string HoldsCurrentFreq;
 string text;
 int numberOfWords = 0;
 int numberOfSentences = 0 ;
-string filename = "input.txt.txt";
-vector<WordFreqPair>wordList;
-int pointer;
+string filename = "input.txt";
+vector<WordFreqPair> wordList;
+int pointer = 0;
 string wordnum;
 string spaces;
 int spacenum;
@@ -35,67 +35,68 @@ int spacenum;
 int main()
 {
 	ifstream fileReader(filename);
-	while (getline(fileReader, text));
-	for (int i = 0; i < text.size(); i++) {
-		if (text[i] == ' ' || text[i] == '!') {
-			numberOfSentences++;
-			numberOfWords++;
-			for (pointer; pointer < i; pointer++) {
-				HoldsCurrentWord = HoldsCurrentWord + text[pointer];
-			}
-			pointer = pointer + i;
-			for (auto savedWord : wordList) {
-				if (savedWord.word == HoldsCurrentWord) {
-					savedWord.count++;
-					wordList[counter].count++;
-					valid = false;
+	while (getline (fileReader, text)) {
+		for (int i = 0; i < text.size(); i++) {
+			if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+				numberOfSentences++;
+				numberOfWords++;
+				for (pointer; pointer < i; pointer++) {
+					HoldsCurrentWord = HoldsCurrentWord + text[pointer];
 				}
-				counter++;
-		}
-			if (valid = true) {
-				wordnum = to_string(numberOfWords);
-				WordFreqPair wordnum(HoldsCurrentWord);
-				wordList.push_back(wordnum);
-			}
-			valid = true;
-			HoldsCurrentWord.clear();
-			counter = 0;
-		}
-		else if (text[i] == ' ' || text[i] == ',') {
-			numberOfWords++;
-			for (pointer; pointer < i; pointer++) {
-				HoldsCurrentWord = HoldsCurrentWord + text[pointer];
-			}
-			pointer = pointer + i;
-			for (auto savedWord : wordList) {
-				if (savedWord.word == HoldsCurrentWord) {
-					savedWord.count++;
-					wordList[counter].count++;
-					valid = false;
+				pointer = i + 1;
+				for (auto savedWord : wordList) {
+					if (savedWord.word == HoldsCurrentWord) {
+						savedWord.count++;
+						wordList[counter].count++;
+						valid = false;
+					}
+					counter++;
 				}
-				counter++;
+				if (valid == true) {
+					wordnum = to_string(numberOfWords);
+					WordFreqPair wordnum(HoldsCurrentWord);
+					wordList.push_back(wordnum);
+				}
+				valid = true;
+				HoldsCurrentWord.clear();
+				counter = 0;
 			}
-			if (valid = true) {
-				wordnum = to_string(numberOfWords);
-				WordFreqPair wordnum(HoldsCurrentWord);
-				wordList.push_back(wordnum);
-			}
-			valid = true;
-			HoldsCurrentWord.clear();
-			counter = 0;
+			else if (text[i] == ' ' || text[i] == ',') {
+				numberOfWords++;
+				for (pointer; pointer < i; pointer++) {
+					HoldsCurrentWord = HoldsCurrentWord + text[pointer];
+				}
+				pointer = i + 1;
+
+				for (auto savedWord : wordList) {
+					if (savedWord.word == HoldsCurrentWord) {
+						savedWord.count++;
+						wordList[counter].count++;
+						valid = false;
+					}
+					counter++;
+				}
+				if (valid == true) {
+					wordnum = to_string(numberOfWords);
+					WordFreqPair wordnum(HoldsCurrentWord);
+					wordList.push_back(wordnum);
+				}
+				valid = true;
+				HoldsCurrentWord.clear();
+				counter = 0;
+			}	
 		}
+		pointer = 0;
 	}
-	cout << "number of sentences is " << numberOfWords << endl ;
-	cout << "number of words is " << numberOfWords << endl; 
-	for (auto savedWord : wordList){
+	cout << "number of sentences is " << numberOfSentences << endl;
+	cout << "number of words is " << numberOfWords << endl;
+	for (auto savedWord : wordList) {
 		spacenum = 8 - savedWord.word.length();
 		for (int i = 0; i <= spacenum; i++) {
 			spaces = spaces + ' ';
-				;
 		}
 		cout << savedWord.word << spaces << savedWord.count << endl;
 		spaces.clear();
-
 	}
 	fileReader.close();
 }
